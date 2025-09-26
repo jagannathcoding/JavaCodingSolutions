327b02207080c69e2f585aeb7d054627edf786f9
Jagannath Deb Gunin <89640442+jagannathcoding@users.noreply.github.com>
2025-09-26 07:55:41 +0530

    aDDED BFS
    aDDED BFS

--- GRAPH/733. Flood Fill algorithm.java
+++ GRAPH/733. Flood Fill algorithm.java
@@ -3,7 +3,7 @@ Flood Fill algorithm.java
   
 //Time Complexity: O(NxM + NxMx4) ~ O(N x M)
 //Space Complexity: O(N x M) + O(N x M)
-
+*********DFS**************
 class Solution
 {
     private void dfs(int row, int col, 
@@ -38,5 +38,46 @@ class Solution
         int delCol[] = {0, +1, 0, -1}; 
         dfs(sr, sc, ans, image, newColor, delRow, delCol, iniColor); 
         return ans;  
+    }
+}
+
+
+*****************BFS*****************
+  class Solution {
+    void bfs(int sr, int sc, int[][] ans, int[][] image, int iniColor, int color, int[] delRow, int[] delCol) {
+        int r = image.length;
+        int c = image[0].length;
+
+        Queue<int[]> q = new LinkedList<>();
+        q.add(new int[]{sr, sc});
+        ans[sr][sc] = color;  // marking starting pixel
+
+        while (!q.isEmpty()) {
+            int[] cell = q.poll();
+            int row = cell[0];
+            int col = cell[1];
+
+           
+            for (int i = 0; i < 4; i++) {
+                int adjRow = row + delRow[i];
+                int adjCol = col + delCol[i];
+
+                if (adjRow >= 0 && adjRow < r && adjCol >= 0 && adjCol < c &&
+                    image[adjRow][adjCol] == iniColor && ans[adjRow][adjCol] != color) {
+
+                    ans[adjRow][adjCol] = color;   // color it
+                    q.add(new int[]{adjRow, adjCol}); // push to queue for BFS
+                }
+            }
+        }
+    }
+    public int[][] floodFill(int[][] image, int sr, int sc, int color) {
+        int initColor=image[sr][sc];
+        int ans[][]=image;
+        int delRow[]={-1,0,+1,0};
+        int delCol[]={0,+1,0,-1};
+        bfs(sr,sc,ans,image,initColor,color,delRow,delCol);
+
+        return ans;
     }
 }
